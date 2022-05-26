23de492481233d429e98acc7e50d048477b56329
Avinash Kumar <97193810+avinash70008@users.noreply.github.com>
2022-05-26 17:36:20 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -25,24 +25,24 @@ app.post("/register",register)
 app.post("/login",login)
 
 
-// app.listen(PORT ,async() => {
+app.listen(PORT ,async() => {
 
-//     try {    
-//         await connect();
-//         console.log("listening port 8080")
-
-//     } catch (e){
-//         console.log(e.message)
-//     }
-// })
-
-
-app.listen(8080, async()=>{
-    try {
+    try {    
         await connect();
-        console.log('Server Connected Port 8080')
-    } catch (error) {
-        console.log(error)
-    }
+        console.log("listening port 8080")
 
+    } catch (e){
+        console.log(e.message)
+    }
 })
+
+
+// app.listen(8080, async()=>{
+//     try {
+//         await connect();
+//         console.log('Server Connected Port 8080')
+//     } catch (error) {
+//         console.log(error)
+//     }
+
+// })
