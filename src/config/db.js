1285eef3bcd64435e570d2184fa297f173aafa09
Avinash Kumar <97193810+avinash70008@users.noreply.github.com>
2022-06-01 18:34:51 +0530

    hi

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connect = async () => {
   try {
     return await mongoose.connect(
-      "mongodb+srv://avinash:avinash1@cluster0.zsvib.mongodb.net/brandhubDatabase?retryWrites=true&w=majority"
+      "mongodb+srv://avinash:avinash1@cluster0.zsvib.mongodb.net/brandhubData?retryWrites=true&w=majority"
     );
   } catch (err) {
     console.log("error form connection :", err.message);
